924238184f9ca401539f72e8f2953499e3e224be
MSI <tulqinurinov005@gmail.com>
2025-11-30 15:48:27 +0500

    changes

--- apps/common/core/permissions/custom_permissions.py
+++ apps/common/core/permissions/custom_permissions.py
@@ -1,15 +1,6 @@
 from rest_framework.permissions import BasePermission
 
 
-# class IsJWTAuthenticated(BasePermission):
-#     """
-#     Faqat middleware orqali authenticate bo'lgan user lar uchun
-#     """
-#
-#     def has_permission(self, request, view):
-#         return bool(request.user and request.role)
-
-
 class AdminPermission(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.role == "ADMIN"
